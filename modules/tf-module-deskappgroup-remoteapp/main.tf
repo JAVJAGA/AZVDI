903b0f93d473238845d9608336956499f1d78d8c
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-03 13:44:06 +0100

    cc

--- modules/tf-module-deskappgroup-remoteapp/main.tf
+++ modules/tf-module-deskappgroup-remoteapp/main.tf
@@ -8,13 +8,6 @@ resource "azurerm_virtual_desktop_application_group" "dag" {
   friendly_name       = var.desktopappfriendlyname
   description         = var.desktopappdescription
 
-tags = {
-    Cloud_Provider = "AZ"
-    Environment = "PRO"
-    Owner      = "TECNOLOGIA-IF"
-    Project    = "SRS_21000191"
-    Project_Description = "MIGRATION_VDI_AZURE"
-    Region = "EUWEST"
-  }
+
 
 }
