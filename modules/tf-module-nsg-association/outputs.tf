adf14bf36432d5e639ea78e48f487bcf5e233a5f
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-03 13:14:28 +0100

    commit

--- modules/tf-module-nsg-association/outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "id" {
-    value = azurerm_subnet_network_security_group_association.nsg_assoc.id
-}
--- modules/tf-module-nsg-association/resources.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id                 = var.subnet_id
-  network_security_group_id = var.networksecuritygroup_id
-}
