64ceba6ef9ee3a28f786045346c854ae7ad04bff
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-06 20:00:39 +0100

    commit

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-    required_providers  {
-        azurerm =   {
-            source  =   "hashicorp/azurerm"
-        }
-    }
-}
-
-# Provider Block
-
-provider "azurerm" {
-    version         =   "~> 2.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    
-    features {}
-}
 
 
 
