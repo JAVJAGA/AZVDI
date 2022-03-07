8274ee858aa0b8b411027472ce31d78484138d01
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-07 20:28:24 +0100

    yy

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-module "resource-group"{
-    source = "./modules/tf-module-resource-group"
+module "hostpool"{
+    source = "./modules/tf-module-hostpool"
     azure_location = "westeurope"
 
-    # Variables for naming convention"
-    company="wz"
-    cloudprovider = "az"
-    environment = "prod"
-    region ="uewe"
-    service = "WVDVDI"
-    instance = "01"
-    tags = {
-        Country = "ES"
-    }
+   
 
 }
 
--- modules/tf-module-hostpool/main.tf
+++ modules/tf-module-hostpool/main.tf
@@ -10,14 +10,7 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
   maximum_sessions_allowed = var.pooledhpmaxsessions
   custom_rdp_properties    = "audiocapturemode:i:1;audiomode:i:0;camerastoredirect:s:*"
 
- tags = {
-    Cloud_Provider = "AZ"
-    Environment = "PRO"
-    Owner      = "TECNOLOGIA-IF"
-    Proyect    = "SRS_21000191"
-    Proyect_Description = "MIGRATION_VDI_AZURE"
-    Region = "EUWEST"
-  }
+ 
 
 registration_info {
     expiration_date = time_rotating.avd_token.rotation_rfc3339
