c8f68f852514a63715ca9fa3d3471b26fbd0e948
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-08 18:15:18 +0100

    save

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 module "hostpool"{
     source = "./modules/tf-module-hostpool"
     azure_location = "westeurope"
+    resourcegroupname  = "RG_hostpool"
+    prefix               = "WVD"
+    hostpool_name         = "Hostpool1"
+    pooledhpmaxsessions  =  "2" #"Max sessions per pooled host"
+    type                 = "pooled"
+     loadbalancertype     = "DepthFirst"
 
    
 
