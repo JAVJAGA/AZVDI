93546c488980e1ed1e94ad7c26057b24d6a8fabb
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-08 21:25:36 +0100

    dd

--- modules/tf-module-hostpool/Outputs.tf
+++ modules/tf-module-hostpool/Outputs.tf
@@ -2,10 +2,10 @@ output "id" {
   value = azurerm_virtual_desktop_host_pool.hostpool.id
 }
 
-output "regtoken" {
-  value = azurerm_virtual_desktop_host_pool.hostpool.registration_info[0].token
-   sensitive = true
-}
+#output "regtoken" {
+ # value = azurerm_virtual_desktop_host_pool.hostpool.registration_info[0].token
+  # sensitive = true
+#}
 
 output "name" {
   value = azurerm_virtual_desktop_host_pool.hostpool.name
