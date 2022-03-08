deefb397e78528dc089b270fedcd9d3bc24bab1d
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-08 20:58:59 +0100

    ddd

--- modules/tf-module-hostpool/main.tf
+++ modules/tf-module-hostpool/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
 
 }
 
- resource "azurerm_virtual_desktop_host_pool_registration_info" {
+ resource "azurerm_virtual_desktop_host_pool_registration_info" "registration_info"{
     expiration_date = time_rotating.avd_token.rotation_rfc3339
   }
 
