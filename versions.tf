2564b7b3dc6e70a9263e8e2e9d41cdeb0adefc85
Javier Garcia Menendez [NE] <jgarciam@kabel.es>
2022-03-08 16:36:14 +0100

    update

--- versions.tf
+++ versions.tf
@@ -1,20 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.83"
-    }
-  }
 
- 
-}
-
-
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  	
-}
 
 
 
