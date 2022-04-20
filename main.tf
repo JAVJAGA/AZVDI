30cb5fec95e11d040886a0dc298c5916e950880b
ESX0004563_prosegur <jgarciam@kabel.es>
2022-04-20 23:06:06 +0200

    uuu

--- main.tf
+++ main.tf
@@ -37,12 +37,33 @@ module "workspace-deskappgroup-association" {
     depends_on = [module.workspace.id, module.deskappgroup.id]
 }
 
+module "virtual-machines" {
+    source = "./modules/tf-module-virtual-machines-vdi" 
+    resourcegroupname                    = var.resourcegroupname_vm
+    azure_location                       = var.azure_location   
+    rdsh_count                           = var.rdsh_count 
+    prefix                               = var.prefix_vm
+    subnet_id                            = var.subnet_id
+    networksecuritygroup_id              = var.networksecuritygroup_id
+    vm_size                              = var.vm_size 
+    vm_image_id                          = var.vm_image_id
+    vm_publisher                         = var.vm_publisher
+    vm_offer                             = var.vm_offer
+    vm_sku                               = var.vm_sku 
+    vm_version                           = var.vm_version
+    vm_timezone                          = var.vm_timezone
+    azure_key_vault_name                 = var.azure_key_vault_name
+    azure_key_vault_resource_group_name  = var.azure_key_vault_resource_group_name
+    localadminpasswordkv_id              = var.localadminpasswordkv_id
+    localadminuserkv_id                  = var.localadminuserkv_id
+    depends_on = [module.hostpool.id]
 
+}
 
 
 locals {
      
      id=module.hostpool.id
-    
+     vm_ids=module.virtual-machines.vm_ids 
 
 }
