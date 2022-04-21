00734b38896daeb1150508420559381d8e2d1bf5
ESX0004563_prosegur <jgarciam@kabel.es>
2022-04-21 12:21:42 +0200

    ddd

--- main.tf
+++ main.tf
@@ -78,16 +78,16 @@ module "extensions" {
     prefix                  = var.prefix_vm
     log_analytics_workspace_id = local.wsloganalytics_id
     log_analytics_workspace_primary_shared_key = local.primary_shared_key
-    hostpoolname            = var.hostpool_name
-    regtoken                = var.regtoken
+    hostpoolname            = local.name
+    regtoken                = local.regtoken
 
 
 }
 
 locals {     
      id=module.hostpool.id
-     #name=module.hostpool.name
-     #regtoken=module.hostpool.regtoken
+     name=module.hostpool.name
+     regtoken=module.hostpool.regtoken
      vm_ids=module.virtual-machines.vm_ids 
      wsloganalytics_id=module.wsp-loganalytics.wsloganalytics_id
      primary_shared_key= module.wsp-loganalytics.primary_shared_key
