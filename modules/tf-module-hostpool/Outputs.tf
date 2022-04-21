4f93a6212e72fd572dccc8622f08a8a977c98d46
ESX0004563_prosegur <jgarciam@kabel.es>
2022-04-21 10:49:21 +0200

    fff

--- modules/tf-module-hostpool/Outputs.tf
+++ modules/tf-module-hostpool/Outputs.tf
@@ -3,7 +3,7 @@ output "id" {
 }
 
 output "regtoken" {
- value = azurerm_virtual_desktop_host_pool.hostpool.registration_info[0].token
+ value = azurerm_virtual_desktop_host_pool.registration_info[0].token
    sensitive = true
 }
 
--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.84"
+      version = ">=2.84"
     }
   }
   required_version = ">= 0.12.24"
